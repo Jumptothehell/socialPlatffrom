bf907a18814f3177601f2d14126c703296e01e8e
Cat say meaw <69138958+Jumptothehell@users.noreply.github.com>
2022-11-17 23:58:34 +0700

    UPDATE: server.js ;add module

---
+++ public/js/feed.js
@@ -0,0 +1,14 @@
+// window.onload =pageLoad;
+
+// function pageLoad(){
+//     // ========== Get name of user =================
+//     var username = "Joy";  //ต้องแก้ Get cookie
+//     console.log(username);
+
+//     document.getElementById("username").innerHTML = username;
+//     document.getElementById('postbutton').onclick =getData();
+// }
+// function getData(){
+//     var msg = document.getElementById("textmsg").value;
+//     document.getElementById("textmsg").value = "";
+// }
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 3000;
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
+const mysql = require('mysql');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, 'public/img/');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+const imageFilter = (req, file, cb) => {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "marrode9202",
+    database: "userdb"
+})
+
+con.connect(err => {
+    if(err) throw(err);
+    else{
+        console.log("MySQL connected");
+    }
+})
+
+const queryDB = (sql) => {
+    return new Promise((resolve,reject) => {
+        // query method
+        con.query(sql, (err,result, fields) => {
+            if (err) reject(err);
+            else
+                resolve(result)
+        })
+    })
+}
+app.listen(port, hostname, () => {
+    console.log(`Server running at   http://${hostname}:${port}/feed.html`); //แก้เป็น register.html
+});
